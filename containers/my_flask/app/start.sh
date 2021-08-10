889e60bc3db03b1689bdb2a5dbaacac6a4ddb71a
William Lees <william@lees.org.uk>
2021-08-10 21:50:24 +0100

    Fixes

--- containers/my_flask/app/start.sh
+++ containers/my_flask/app/start.sh
@@ -24,7 +24,7 @@ echo "starting cron"
 service cron start
 echo "starting gunicorn"
 export PYTHONUNBUFFERED=1
-exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=10 --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app 
+exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=2 --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app 
 
 while true; do sleep 60; done
 
