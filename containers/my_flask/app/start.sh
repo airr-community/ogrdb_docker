070cf9563811dd587b22e969894d188177c6a1bf
William Lees <william@lees.org.uk>
2021-08-24 10:50:29 +0100

    Changes for decoupled infra

--- containers/my_flask/app/start.sh
+++ containers/my_flask/app/start.sh
@@ -10,7 +10,7 @@ cd /app
 echo "starting cron"
 service cron start
 
-if [ ! -f /attachments/noflask ]; then
+if [ ! -f /ogre/attachments/noflask ]; then
   echo "starting gunicorn"
   export PYTHONUNBUFFERED=1
   exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=2 --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app 
