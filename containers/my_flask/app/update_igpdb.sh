8c3ba4fea09c5aee44dcdd9a4311257ad35ecc8c
William Lees <william@lees.org.uk>
2021-08-26 15:18:10 +0100

    Tracker changes

--- containers/my_flask/app/update_igpdb.sh
+++ containers/my_flask/app/update_igpdb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /ogre/static/docs
+cd /ogre/imgt_files
 wget -O igpdb.fasta --post-data "paper=0&study=0&type=All&file=true&submit=Download" http://cgi.cse.unsw.edu.au/~ihmmune/IgPdb/download.php
--- containers/my_flask/app/update_vdjbase.sh
+++ containers/my_flask/app/update_vdjbase.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /ogre/static/docs
+cd /ogre/imgt_files
 wget --no-check-certificate -O vdjbase.fasta "https://www.vdjbase.org/data/Alleles?name=&gene=&similar=&show_imgt=on&show_novel=on&download="
