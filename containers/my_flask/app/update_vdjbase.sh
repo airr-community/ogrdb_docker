822bc789390eb1544820ce87f511f5f27ec8befe
William Lees <william@lees.org.uk>
2021-09-08 15:28:27 +0100

    Temp url for vdjbase query

--- containers/my_flask/app/update_vdjbase.sh
+++ containers/my_flask/app/update_vdjbase.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /ogre/imgt_files
-wget --no-check-certificate -O vdjbase.json "https://www.vdjbase.org/api/repseq/all_novels"
+wget --no-check-certificate -O vdjbase.json "https://vdjbasetest.vdjbase.org/admin/api/repseq/all_novels"
