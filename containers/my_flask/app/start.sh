56940ff957addf70f9673df94cd842d4d45f6b25
William Lees <william@lees.org.uk>
2021-11-24 10:39:21 +0000

    start script updates

--- containers/my_flask/app/start.sh
+++ containers/my_flask/app/start.sh
@@ -10,18 +10,23 @@ cd /app
 echo "starting cron"
 service cron start
 
+echo "updating reference files"
+./update_imgt.sh
+./update_igpdb.sh
+./update_vdjbase.sh
 
-echo "migrating database"
-
-if [ ! -f migrations/README ]; then
-  echo "creating migrations database"
-  rm -rf migrations
-  flask db init
-fi
-flask db migrate
-flask db upgrade
 
 if [ ! -f /ogre/attachments/noflask ]; then
+echo "migrating database"
+
+  if [ ! -f migrations/README ]; then
+    echo "creating migrations database"
+    rm -rf migrations
+    flask db init
+  fi
+  flask db migrate
+  flask db upgrade
+
   echo "starting gunicorn"
   export PYTHONUNBUFFERED=1
   exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=2 --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app --pid /app/gunicorn.pid
