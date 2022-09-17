8917032db57198703e9b6944ad7a224f636c3af0
William Lees <william@lees.org.uk>
2022-09-17 17:14:47 +0100

    Add cronjob for zenodo

--- containers/my_flask/app/zenodo_dump.sh
+++ containers/my_flask/app/zenodo_dump.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # Modify this file to use the required zenodo toek and id, and the ogrdb password
 cd /tmp
 /usr/bin/mysqldump -h mariadb -P 3306 -u ogrdb -p75CVi8SFqsjifY --ignore-table=ogrdb.alembic_version --ignore-table=ogrdb.role --ignore-table=ogrdb.roles_submissions --ignore-table=ogrdb.roles_users --ignore-table=ogrdb.user ogrdb >ogrdb_dump.sql  
 /bin/tar -cvzf ogrdb_archive.tgz ogrdb_dump.sql /ogre/attachments/*
-/usr/local/bin/python /app/zenodo_new_version.py access_tocken deposition_id zonodo_dump.sql 0
+/usr/local/bin/python /app/zenodo_new_version.py access_tocken deposition_id ogrdb_archive.tgz 0
