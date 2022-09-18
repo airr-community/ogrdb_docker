40423fd8b629a14df30da7847e31ba555be00a98
William Lees <william@lees.org.uk>
2022-09-18 10:23:24 +0100

    FIx typo

--- containers/my_flask/app/zenodo_dump_sample.sh
+++ containers/my_flask/app/zenodo_dump_sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Modify this file to use the required zenodo toek and id, and the ogrdb password
+# Modify this file to use the required zenodo token and id, and the ogrdb password
 cd /tmp
 /usr/bin/mysqldump -h mariadb -P 3306 -u ogrdb -p75CVi8SFqsjifY --ignore-table=ogrdb.alembic_version --ignore-table=ogrdb.role --ignore-table=ogrdb.roles_submissions --ignore-table=ogrdb.roles_users --ignore-table=ogrdb.user ogrdb >ogrdb_dump.sql  
 /bin/tar -cvzf ogrdb_archive.tgz ogrdb_dump.sql /ogre/attachments/*
