042c6647a49b4242ffc8fa62275f13fc4edf6991
William Lees <william@lees.org.uk>
2022-09-27 10:33:19 +0100

    Updates to periodic zenodo database dump

--- containers/my_flask/app/zenodo_dump_sample.sh
+++ containers/my_flask/app/zenodo_dump_sample.sh
@@ -3,4 +3,4 @@
 cd /tmp
 /usr/bin/mysqldump -h mariadb -P 3306 -u ogrdb -p75CVi8SFqsjifY --ignore-table=ogrdb.alembic_version --ignore-table=ogrdb.role --ignore-table=ogrdb.roles_submissions --ignore-table=ogrdb.roles_users --ignore-table=ogrdb.user ogrdb >ogrdb_dump.sql  
 /bin/tar -cvzf ogrdb_archive.tgz ogrdb_dump.sql /ogre/attachments/*
-/usr/local/bin/python /app/zenodo_new_version.py access_tocken deposition_id ogrdb_archive.tgz 0
+/usr/local/bin/python /app/zenodo.py deposition_id ogrdb_archive.tgz 0
