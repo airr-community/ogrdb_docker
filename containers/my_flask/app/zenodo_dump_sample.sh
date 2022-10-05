8d37a878781ae7c00711b9d14c64bd4f0cfa4163
William Lees <william@lees.org.uk>
2022-10-05 11:48:21 +0100

    Fix error in example

--- containers/my_flask/app/zenodo_dump_sample.sh
+++ containers/my_flask/app/zenodo_dump_sample.sh
@@ -3,4 +3,5 @@
 cd /tmp
 /usr/bin/mysqldump -h mariadb -P 3306 -u ogrdb -p75CVi8SFqsjifY --ignore-table=ogrdb.alembic_version --ignore-table=ogrdb.role --ignore-table=ogrdb.roles_submissions --ignore-table=ogrdb.roles_users --ignore-table=ogrdb.user ogrdb >ogrdb_dump.sql  
 /bin/tar -cvzf ogrdb_archive.tgz ogrdb_dump.sql /ogre/attachments/*
-/usr/local/bin/python /app/zenodo.py deposition_id ogrdb_archive.tgz 0
+cd /app
+/usr/local/bin/python zenodo.py deposition_id /tmp/ogrdb_archive.tgz 0
