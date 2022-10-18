cb4bb8526425f698f8c48f0679bdabc96f212e54
William Lees <william@lees.org.uk>
2022-10-18 10:16:28 +0100

    Update backup for healthchecks

--- containers/my_flask/app/do_backup.sh
+++ containers/my_flask/app/do_backup.sh
@@ -5,6 +5,9 @@
 BACKUP_DIR=/backup
 TEMP_DIR=/tmp
 
+slug=ogrdb-backup
+python /app/healthchecks.py $slug start
+
 # Precautionary cleanup
 mkdir -p $BACKUP_DIR/incoming
 mkdir -p $BACKUP_DIR/temp
@@ -16,6 +19,35 @@ rm -rf $BACKUP_DIR/temp/*
 
 # Dump MySQL tables
 mysqldump --all-databases -h mariadb -P 3306 -u ogrdb -p75CVi8SFqsjifY >/config/log/sqldump
+
+updatedfile=/config/log/sqldump
+minsize=1000
+
+if ! [ $(find "$updatedfile") ]
+then
+    python /app/healthchecks.py infra-goaccess fail -m "$updatedfile not created"
+	exit
+else
+	echo "$updatedfile exists"
+fi	
+
+if [ $(find "$updatedfile" -mmin +60) ]
+then
+	python /app/healthchecks.py vdjbase-backups -m "$updatedfile not updated"
+	exit
+else
+	echo "$updatedfile updated"
+fi
+
+backupsize=$(find "$updatedfile" -printf "%s")
+
+if [ $backupsize -lt $minsize ]
+then
+	python /app/healthchecks.py vdjbase-backups fail -m "$updatedfile is implausibly small"
+	exit
+else
+	echo "$updatedfileis a reasonable size"
+fi
 
 # Backup logs and config
 
@@ -33,9 +65,40 @@ cd $BACKUP_DIR/temp
 # Compress tables and files
 tar -cvzf $BACKUP_DIR/incoming/archive.tgz *
 
+updatedfile=$BACKUP_DIR/incoming/archive.tgz
+minsize=1000
+
+if ! [ $(find "$updatedfile") ]
+then
+    python /app/healthchecks.py infra-goaccess fail -m "$updatedfile not created"
+	exit
+else
+	echo "$updatedfile exists"
+fi	
+
+if [ $(find "$updatedfile" -mmin +60) ]
+then
+	python /app/healthchecks.py vdjbase-backups -m "$updatedfile not updated"
+	exit
+else
+	echo "$updatedfile updated"
+fi
+
+backupsize=$(find "$updatedfile" -printf "%s")
+
+if [ $backupsize -lt $minsize ]
+then
+	python /app/healthchecks.py vdjbase-backups fail -m "$updatedfile is implausibly small"
+	exit
+else
+	echo "$updatedfileis a reasonable size"
+fi
+
 # Cleanup
 rm -rf $BACKUP_DIR/temp/*
 
 # Run backup rotate
 cd $BACKUP_DIR
 bash /app/rotate.sh
+
+python /app/healthchecks.py $slug success
